d1111c75e43fd6aa9c21e087886d8cce3a6bcfaa
olajanz <aleksandra.janz@protonmail.com>
2020-05-13 10:33:54 +0200

    Test case exercise

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h2>Event</h2>
+    <h2>{{ event.title }}</h2>
+    <h3>{{ date }}</h3>
+    <p>{{ event.location }}</p>
+    <p>{{ event.details }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["event"],
+  computed: {
+    date() {
+      let cardDate = new Date(this.event.date);
+      return cardDate.toDateString();
+    },
+  },
+};
 </script>
 
 <style scoped></style>

ab479510bdbe45e45359ce07c31f289cd431c737
olajanz <aleksandra.janz@protonmail.com>
2020-05-15 16:12:46 +0200

    add new css and input fieelds, todo: testing

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -8,6 +8,27 @@
         name="title"
         placeholder="Add a Title"
       />
+
+      <input
+        v-model="event.details"
+        type="text"
+        name="details"
+        placeholder="Add some details"
+      />
+
+      <input
+        v-model="event.location"
+        type="text"
+        name="location"
+        placeholder="Add a location"
+      />
+
+      <input
+        v-model="event.date"
+        type="date"
+        name="date"
+        placeholder="Date: mm/dd/yyyy"
+      />
       <input type="submit" value="Create" />
     </form>
   </div>
@@ -20,21 +41,31 @@ export default {
   data() {
     return {
       event: {
-        title: ""
-      }
+        title: "",
+        details: "",
+        location: "",
+        date: "",
+      },
     };
   },
   methods: {
     async submit() {
       try {
-        const res = await createEvent(this.event);
-        console.log("Created new event", res.data);
+        await createEvent(this.event);
+        this.$router.push({ name: "dashboard" });
       } catch (error) {
-        console.error(error);
+        alert("Couldn't create an entry, check your input");
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  display: grid;
+  gap: 2em;
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
